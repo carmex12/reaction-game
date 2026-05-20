9890741b5a055acda488e17c004c2a7aecc4b665
Justin Elak <justin.elak@gmail.com>
2026-05-19 20:33:03 -0400

    Add back reaction_game.ino

---
+++ reaction_game.ino
@@ -0,0 +1,75 @@
+int leds[6] = {5, 18, 19, 21, 22, 23};
+const int buttonPin = 4;
+int buttonState = LOW;
+bool animation = true;
+int currentLed = 0;
+unsigned long lastStep = 0;
+const int stepTime = 100;
+int chosenLed = 0;
+
+void setup() {
+  for (int i = 0; i < 6; i++) {
+    pinMode(leds[i], OUTPUT);
+  }
+  pinMode(buttonPin, INPUT_PULLUP);
+  // randomSeed(analogRead(34));
+  chosenLed = chooseLed();
+
+}
+
+int chooseLed() {
+  int chosen = leds[random(0,6)];
+  for (int i = 0; i<3; i++) {
+    digitalWrite(chosen, HIGH);
+    delay(250);
+    digitalWrite(chosen, LOW);
+    delay(250);
+  }
+  return chosen;
+}
+
+void allOff() {
+  for (int i = 0; i < 6; i++)  {
+    digitalWrite(leds[i], LOW);
+  }
+}
+
+void allLeds(int val) {
+  for (int i = 0; i < 6; i++) {
+    digitalWrite(leds[i], val);
+  }
+}
+
+void moveLight() {
+  if(millis() - lastStep >= stepTime) {
+    allOff();
+
+    digitalWrite(leds[currentLed], HIGH);
+
+    currentLed++;
+
+    if (currentLed >= 6) {
+      currentLed = 0;
+    }
+
+    lastStep = millis();
+  }
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == LOW) {
+    animation = false;
+  }
+  if (animation) {
+    moveLight();
+  } else {
+    allOff();
+    if (leds[currentLed] == chosenLed) {
+      allLeds(HIGH);
+    } else {
+      digitalWrite(leds[currentLed], HIGH);
+    }
+  }
+
+}
